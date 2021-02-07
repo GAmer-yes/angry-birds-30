161272cb3da331a9a15661934c1c7352b530529c
GAmer-yes <70022849+GAmer-yes@users.noreply.github.com>
2021-02-07 13:58:19 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,13 +5,14 @@ const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1,pig3;
-var backgroundImg,platform;
+var backgroundImage,platform;
 var bird, slingshot;
-var gamestate = "onSling";
-
+var bg="sprites/bg.png"
+var gameState = "onSling";
+var score=0
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+   getbackroundImage();
 }
 
 function setup(){
@@ -45,18 +46,23 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImage)
+    background(backgroundImage);
+    textSize(30);
+    text("Score "+score,10,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -70,19 +76,33 @@ function draw(){
 }
 
 function mouseDragged(){
-    if(gamestate!=="Launched"){
-    Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
+    if (gameState!=="launched"){
+        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
     }
 }
 
 
 function mouseReleased(){
     slingshot.fly();
-    gamestate="Launched";
+    gameState = "launched";
 }
 
 function keyPressed(){
     if(keyCode === 32){
-        slingshot.attach(bird.body);
+       // slingshot.attach(bird.body);
     }
+}
+
+async function getbackroundImage(){
+    var response=await fetch("http://worldtimeapi.org/api/timezone/America/New_York");
+    var responseJson=await response.json();
+    var dateTime=responseJson.datetime;
+    var hour=dateTime.slice(11,13);
+    if(hour>=06 && hour<=19){
+        bg="sprites/bg.png";
+    }
+    else{
+        bg="sprites/bg2.jpg";
+    }
+    backgroundImage=loadImage(bg);
 }
